340aca8878c8cb0dcc72f133faaa3b1c5699a57a
Johnson Mao <tutelary.maomao@gmail.com>
2023-01-15 16:09:33 +0800

    feat: action 調整 & deploy demo

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 
@@ -11,41 +11,46 @@ interface IMonacoEditorPlugin {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        eslintPlugin({ cache: false }),
-        (monacoEditorPlugin as unknown as IMonacoEditorPlugin).default({
-            languageWorkers: ['editorWorkerService', 'json']
-        })
-    ],
-    test: {
-        globals: true,
-        environment: 'jsdom'
-    },
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'lib/index.ts'),
-            name: 'json-schema',
-            fileName: 'json-schema'
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+        base: env.VITE_BASE,
+        plugins: [
+            vue(),
+            eslintPlugin({ cache: false }),
+            (monacoEditorPlugin as unknown as IMonacoEditorPlugin).default({
+                languageWorkers: ['editorWorkerService', 'json']
+            })
+        ],
+        test: {
+            globals: true,
+            environment: 'jsdom'
         },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue'
+        build: {
+            lib: !!env.VITE_DEMO && {
+                entry: resolve(__dirname, 'lib/index.ts'),
+                name: 'json-schema',
+                fileName: 'json-schema'
+            },
+            rollupOptions: !!env.VITE_DEMO && {
+                external: ['vue'],
+                output: {
+                    globals: {
+                        vue: 'Vue'
+                    }
                 }
             }
+        },
+        resolve: {
+            alias: {
+                '@': '/src'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: 8080,
+            open: true
         }
-    },
-    resolve: {
-        alias: {
-            '@': '/src'
-        }
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 8080,
-        open: true
     }
 })
